e38bf5c688e9ba59deeeec59a172de7b9cf519b9
Geoff at SAE <g.hill@sae.edu>
2020-08-18 09:49:59 +1000

    Initial Commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public Transform spawnPoint1, spawnPoint2, spawnPoint3;
+    public GameObject milkBottlePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(spawnPoint2.position);
+
+        //GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+        //Instantiate(cube, spawnPoint2.position, Quaternion.identity);
+        MoreBottles();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MoreBottles()
+    {
+        Instantiate(milkBottlePrefab, new Vector3(spawnPoint1.position.x, spawnPoint1.position.y, spawnPoint1.position.z), Quaternion.identity);
+        Instantiate(milkBottlePrefab, new Vector3(spawnPoint2.position.x, spawnPoint2.position.y, spawnPoint2.position.z), Quaternion.identity);
+        Instantiate(milkBottlePrefab, new Vector3(spawnPoint3.position.x, spawnPoint3.position.y, spawnPoint3.position.z), Quaternion.identity);
+
+    }
+}
---
+++ Assets/Scripts/WhatHaveICollidedWith.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhatHaveICollidedWith : MonoBehaviour
+{
+    Renderer theRend;
+    public bool isHot = false;
+    Vector3 originalPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theRend = GetComponent<Renderer>();
+        originalPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("I have collided with " + collision.gameObject.name);
+
+
+        if(collision.gameObject.name == "Lava")
+        {
+            //it is the lava, make red
+            //Debug.Log("the evaluation of lava and is not hot is true");
+            //theRend.material.color = Color.red;
+            //isHot = true;
+
+
+        }
+
+    }
+
+    public void ResetTheBottle()
+    {
+        transform.position = originalPosition;
+
+
+    }
+}
